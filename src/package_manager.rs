d2f92f05076fe1e2f46c9d1692b5ace94dc1812e
hrs <h.r.szpuk@outlook.com>
2022-08-24 22:50:33 +0100

    Added more flags and pm functions
    Added every flag planned in the README into Cli.
    Just figured I'd get it out of the way now instead of needing to
    go back to the cli and make huge modification in the future.
    
    Added every function to the package_manager.rs.
    This is quite a lot. I imagine, other functions like database, and utilities will require their own file as package_manger.rs will be too big.
    
    Changelog:
    - Added more flag options to the cli
    - Added functions to package_manager.rs

--- src/package_manager.rs
+++ src/package_manager.rs
@@ -10,3 +10,35 @@ pub fn update() {
 pub fn remove(args: Vec<String>) {
 
 }
+
+pub fn eliminate(args: Vec<String>) {
+
+}
+
+pub fn clean() {
+
+}
+
+pub fn search(args: Vec<String>) {
+
+}
+
+pub fn list_packages() {
+
+}
+
+pub fn list_dependencies() {
+
+}
+
+pub fn list_package_dependencies(args: Vec<String>) {
+
+}
+
+pub fn local_install(args: Vec<String>) {
+
+}
+
+pub fn info(args: Vec<String>) {
+
+}
